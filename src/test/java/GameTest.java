74bf86fafa0921b2af8f2207188b3a131bc711a8
tedasi <tenorio.felipe.dasilva@gmail.com>
2025-11-29 21:09:16 +0100

    test victoria
    el jugador gana cuando llega a la casilla de salida y todas las casillas no son '0'

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -68,6 +68,32 @@ public class GameTest {
         assertEquals(0, game.getPlayerY(), "El jugador no debería haber cambiado de Y");
     }
 
+    @Test
+    void testWinningCondition() {
+        Game game = new Game();
+        game.startNewGame();
+        Level level = game.getLevel();
 
+        // definimos la Salida en (0, 1)
+        level.setExit(0, 1);
+
+        // iluminamos todas las casillas excepto la de la Salida a la fuerza
+        for (int i = 0; i < level.getSize(); i++) {
+            for (int j = 0; j < level.getSize(); j++) {
+                level.setCell(i, j, '*');
+            }
+        }
+        // apagamos la casilla de la Salida
+        level.setCell(0, 1, 'O');
+
+        // ponemos al jugador al lado (0, 0)
+        game.setPlayerPosition(0, 0);
+
+        // lo movemos a la salida
+        game.movePlayer(Direction.DOWN); // (0,0) -> (0,1)
+
+        
+        assertEquals(GameStatus.WON, game.getStatus(), "El juego debería ganarse al llegar a la salida con todo iluminado");
+    }
 }
 
